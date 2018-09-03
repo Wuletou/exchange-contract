e71c4026392762e76090f8b74d70aa34545aeea9
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-09-03 17:27:48 +0300

    add trade method

--- exchange.cpp
+++ exchange.cpp
@@ -23,14 +23,49 @@ namespace eosio {
    }
 
    void exchange::on( const trade& t ) {
-//      require_auth( t.seller );
-//      eosio_assert( t.sell.is_valid(), "invalid sell amount" );
-//      eosio_assert( t.sell.amount > 0, "sell amount must be positive" );
-//      eosio_assert( t.receive.is_valid(), "invalid receive amount" );
-//      eosio_assert( t.receive.amount >= 0, "receive amount cannot be negative" );
+      require_auth( t.seller );
+      eosio_assert( t.sell.is_valid(), "invalid sell amount" );
+      eosio_assert( t.sell.amount > 0, "sell amount must be positive" );
+      eosio_assert( t.receive.is_valid(), "invalid receive amount" );
+      eosio_assert( t.receive.amount >= 0, "receive amount cannot be negative" );
 
-//      auto receive_symbol = t.receive.get_extended_symbol();
-//      eosio_assert( t.sell.get_extended_symbol() != receive_symbol, "invalid conversion" );
+      auto receive_symbol = t.receive.get_extended_symbol();
+      eosio_assert( t.sell.get_extended_symbol() != receive_symbol, "invalid conversion" );
+      // todo: check that sender's balance is enough
+
+      market_state market( _this_contract, t.market, _accounts );
+
+      auto temp   = market.exstate;
+      auto output = temp.convert( t.sell, receive_symbol );
+
+      while( temp.requires_margin_call() ) {
+         market.margin_call( receive_symbol );
+         temp = market.exstate;
+         output = temp.convert( t.sell, receive_symbol );
+      }
+      market.exstate = temp;
+
+      print( name{t.seller}, "   ", t.sell, "  =>  ", output, "\n" );
+
+      if( t.receive.amount != 0 ) {
+         eosio_assert( t.receive.amount <= output.amount, "unable to fill" );
+      }
+
+      _accounts.adjust_balance( t.seller, -t.sell, "sold" );
+      _accounts.adjust_balance( t.seller, output, "received" );
+
+      if( market.exstate.supply.amount != market.initial_state().supply.amount ) {
+         auto delta = market.exstate.supply - market.initial_state().supply;
+
+         _excurrencies.issue_currency( { .to = _this_contract,
+                                               .quantity = delta,
+                                               .memo = string("") } );
+      }
+
+      /// TODO: if pending order start deferred trx to fill it
+
+      market.save();
+
    }
 
    void exchange::createx( account_name    creator,
@@ -44,6 +79,7 @@ namespace eosio {
       eosio_assert( quote_deposit.amount > 0, "quote deposit must be positive" );
       eosio_assert( base_deposit.get_extended_symbol() != quote_deposit.get_extended_symbol(),
                     "must exchange between two different currencies" );
+      // todo: check that sender's balance is enough
 
       print( "base: ", base_deposit.get_extended_symbol(), '\n');
       print( "quote: ",quote_deposit.get_extended_symbol(), '\n');
